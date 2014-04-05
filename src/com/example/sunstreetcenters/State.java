07cc7a994dccb8eda9d45896df6f5f8bbc226d70
briandeanda <briandeanda2@gmail.com>
2014-04-05 12:10:38 -0700

    Added poll with checkbox functionality.

---
+++ src/com/example/sunstreetcenters/State.java
@@ -0,0 +1,21 @@
+package com.example.sunstreetcenters;
+
+public class State {
+	private String name;
+	private String capital;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	
+}
